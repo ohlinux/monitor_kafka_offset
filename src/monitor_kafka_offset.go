d3c9630da89a3aa8b2cc01cff56e20de0416e065
zhangjian12 <zhangjian12@baidu.com>
2016-03-06 22:16:27 +0800

    add multi cluster monitor

--- src/monitor_kafka_offset.go
+++ src/monitor_kafka_offset.go
@@ -2,22 +2,31 @@ package main
 
 import (
 	"bufio"
-	//"bytes"
 	"flag"
+	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"runtime"
 	"strconv"
 	"strings"
 	"time"
 
 	log "github.com/cihub/seelog"
-	"github.com/robfig/config"
+	//"github.com/robfig/config"
 	"github.com/samuel/go-zookeeper/zk"
+	"gopkg.in/yaml.v2"
 )
 
-const AppVersion = "Version 0.0.201512141923"
+const AppVersion = "Version 0.1.20160305"
+
+// Version 0.0.201512141923  : the first version
+// Version 0.1.20160305 : 增加多个集群的支持, 去掉log的配置,修复bug
+
+var cmdPath string
+var c Conf
 
 var (
 	SUDO_USER  = os.Getenv("SUDO_USER")
@@ -31,7 +40,7 @@ var (
 )
 
 type MKafka struct {
-	ZKList    string
+	ZKList    []string
 	ZNode     string
 	ToolDir   string
 	LogFormat string
@@ -39,6 +48,7 @@ type MKafka struct {
 	Consumers []string
 	zkc       *zk.Conn
 	SetLag    map[string]int64
+	Cluster   string
 }
 
 type Offset struct {
@@ -46,14 +56,46 @@ type Offset struct {
 	Topic   string
 	Pid     int
 	Offset  int64
-	logSize int64
+	LogSize int64
 	Lag     int64
 	Owner   string
+}
+
+type Conf struct {
+	ENV     Env              `yaml:"ENV"`
+	LOG     Log              `yaml:"LOG"`
+	Monitor []string         `yaml:"Monitor"`
+	Cluster map[string]Kafka `yaml:"Cluster"`
+}
+
+type Env struct {
+	KafkaToolDir string `yaml:"KafkaToolDir"`
+	JAVAHOME     string `yaml:"JAVAHOME"`
+}
+
+type Log struct {
+	LogFormat string `yaml:"LogFormat"`
+}
+
+type Kafka struct {
+	ZNode  string   `yaml:"ZNode"`
+	ZKList []string `yaml:"ZKList"`
+	SetLag []Lag    `yaml:"SetLag"`
+}
+
+type Lag struct {
+	Consumer string `yaml:"Consumer"`
+	Lag      int64  `yaml:"Lag"`
 }
 
 func main() {
 
 	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	//日志记录
+	cmdPath, _ = filepath.Abs(filepath.Dir(os.Args[0]))
+	defer log.Flush()
+	syncLogger()
 
 	//获取相关信息
 	kafka := NewMKafka()
@@ -64,71 +106,78 @@ func main() {
 	}
 }
 
-func realMain(m *MKafka) {
+func realMain(m []MKafka) {
 
 	//根据consumer检查offset
+	for _, v := range m {
 
-	m.Consumers = GetConsumsers(m.ZNode, m.zkc)
+		v.Consumers = GetConsumsers(v.ZNode, v.zkc)
 
-	for _, consumer := range m.Consumers {
-		log.Infof("get consumser %v", consumer)
-		go m.checkConsumer(consumer)
-	}
-}
-
-func NewMKafka() *MKafka {
-	flag.Parse()
-	//配置文件
-	conf, err := config.ReadDefault(*configFile)
-	checkErr(1, err)
-
-	//日志记录
-	logConf, _ := conf.String("Log", "LogConf")
-	logger, _ := log.LoggerFromConfigAsFile(logConf)
-	log.ReplaceLogger(logger)
-
-	zklist, err := conf.String("Monitor", "ZKList")
-	checkErr(1, err)
-	zknode, err := conf.String("Monitor", "ZNode")
-	checkErr(1, err)
-
-	tooldir, err := conf.String("Monitor", "KafkaToolDir")
-	checkErr(1, err)
-	logformat, err := conf.String("Log", "LogFormat")
-	checkErr(1, err)
-
-	lags, err := conf.String("Monitor", "SetLag")
-	checkErr(1, err)
-
-	setlag := make(map[string]int64)
-
-	topic_lags := strings.Split(lags, ",")
-	for _, v := range topic_lags {
-		k := strings.Split(v, ":")
-		if len(k) == 2 {
-			setlag[k[0]], err = strconv.ParseInt(k[1], 10, 64)
-			checkErr(1, err)
+		for _, consumer := range v.Consumers {
+			log.Infof("get consumser %v", consumer)
+			go v.checkConsumer(consumer)
 		}
+
 	}
-
-	zks := strings.Split(zklist, ",")
-	zkcon, err := NewZk(zks)
-	checkErr(1, err)
-
-	info := &MKafka{
-		ZKList:    zklist,
-		ZNode:     zknode,
-		ToolDir:   tooldir,
-		LogFormat: logformat,
-		zkc:       zkcon,
-		SetLag:    setlag,
-	}
-
-	//info.Consumers = GetConsumsers(zknode, zkcon)
-	return info
 }
 
-func (mk *MKafka) checkConsumer(consumer string) {
+func NewMKafka() []MKafka {
+	flag.Parse()
+
+	//配置文件
+	var confFile string
+	if !filepath.IsAbs(*configFile) {
+		confFile = fmt.Sprintf("%s/../%s", cmdPath, *configFile)
+	}
+	yamlFile, err := ioutil.ReadFile(confFile)
+	if err != nil {
+		panic(err)
+	}
+
+	err = yaml.Unmarshal(yamlFile, &c)
+	if err != nil {
+		panic(err)
+	}
+
+	if len(c.Monitor) == 0 {
+		fmt.Printf("Not found monitor cluser ,Please check conf %s\n", confFile)
+		os.Exit(1)
+	}
+
+	var infos []MKafka
+
+	for _, m := range c.Monitor {
+
+		zkcon, err := NewZk(c.Cluster[m].ZKList)
+		//TODO: disable panic
+		checkErr(1, err)
+		if err != nil {
+			panic(err)
+		}
+
+		lags := make(map[string]int64)
+		for _, v := range c.Cluster[m].SetLag {
+			lags[v.Consumer] = v.Lag
+		}
+
+		f := MKafka{
+			ZKList:    c.Cluster[m].ZKList,
+			ZNode:     c.Cluster[m].ZNode,
+			ToolDir:   c.ENV.KafkaToolDir,
+			LogFormat: c.LOG.LogFormat,
+			zkc:       zkcon,
+			SetLag:    lags,
+			Cluster:   m,
+		}
+		log.Infof("Load yaml conf %+v", f)
+		infos = append(infos, f)
+	}
+
+	log.Infof("parse config file : %v", infos)
+	return infos
+}
+
+func (mk MKafka) checkConsumer(consumer string) {
 
 	topics := GetTopicsWithConsumser(mk.ZNode, mk.zkc, consumer)
 
@@ -139,11 +188,11 @@ func (mk *MKafka) checkConsumer(consumer string) {
 
 }
 
-func (mk *MKafka) getConsumerOffset(consumer, topic string) {
+func (mk MKafka) getConsumerOffset(consumer, topic string) {
 	//var stdout, outerr bytes.Buffer
 	// export JAVA_HOME=/usr/java/jdk1.7.0_67-cloudera
-	os.Setenv("JAVA_HOME", "/usr/java/jdk1.7.0_67-cloudera")
-	cmd := exec.Command(mk.ToolDir+"/kafka-consumer-offset-checker.sh", "--zookeeper", mk.ZKList+mk.ZNode, "--topic", topic, "--group", consumer)
+	os.Setenv("JAVA_HOME", c.ENV.JAVAHOME)
+	cmd := exec.Command(mk.ToolDir+"/kafka-consumer-offset-checker.sh", "--zookeeper", strings.Join(mk.ZKList, ",")+mk.ZNode, "--topic", topic, "--group", consumer)
 
 	//命令执行
 	stdout, err := cmd.StdoutPipe()
@@ -171,44 +220,54 @@ func (mk *MKafka) getConsumerOffset(consumer, topic string) {
 				continue
 			}
 
-			pid, err := strconv.Atoi(words[2])
-			checkErr(1, err)
-
-			offset, err := strconv.ParseInt(words[3], 10, 64)
-			checkErr(1, err)
-
-			logsize, err := strconv.ParseInt(words[4], 10, 64)
-			checkErr(1, err)
-
-			lag, err := strconv.ParseInt(words[5], 10, 64)
-			checkErr(1, err)
-
 			data := &Offset{
-				Group:   words[0],
-				Topic:   words[1],
-				Pid:     pid,
-				Offset:  offset,
-				logSize: logsize,
-				Lag:     lag,
-				Owner:   words[6],
+				Group: words[0],
+				Topic: words[1],
+				Owner: words[6],
 			}
 
-			total_messages += logsize
-			total_lag += lag
+			if pid, err := strconv.Atoi(words[2]); err == nil {
+				data.Pid = pid
+			} else {
+				log.Errorf("change %s to int error %v", words[2], err)
+				continue
+			}
+
+			if offset, err := strconv.ParseInt(words[3], 10, 64); err == nil {
+				data.Offset = offset
+			} else {
+				log.Errorf("change %s to int64 error %v", words[3], err)
+				continue
+			}
+
+			if logsize, err := strconv.ParseInt(words[4], 10, 64); err == nil {
+				data.LogSize = logsize
+			} else {
+				log.Errorf("change %s to int64 error %v", words[4], err)
+				continue
+			}
+
+			if lag, err := strconv.ParseInt(words[5], 10, 64); err == nil {
+				data.Lag = lag
+			} else {
+				log.Errorf("change %s to int64 error %v", words[5], err)
+				continue
+			}
+
+			total_messages += data.LogSize
+			total_lag += data.Lag
 			partition += 1
 
 			if m, ok := mk.SetLag[consumer]; ok {
 				if data.Lag > m {
-					log.Warnf(mk.LogFormat, data.Topic, data.Group, data.Pid, data.logSize, data.Offset, data.Lag)
+					log.Warnf("[Consumer Detail Info] "+mk.LogFormat, data.Topic, data.Group, data.Pid, data.LogSize, data.Offset, data.Lag, mk.Cluster)
 				}
 			} else if data.Lag > 50 {
-				//"topic:%s consumer:%s partition:%d logsize:%s offset:%s lag:%s"
-				log.Warnf(mk.LogFormat, data.Topic, data.Group, data.Pid, data.logSize, data.Offset, data.Lag)
-
+				log.Warnf("[Consumer Detail Info] "+mk.LogFormat, data.Topic, data.Group, data.Pid, data.LogSize, data.Offset, data.Lag, mk.Cluster)
 			}
 		}
 	}
-	log.Infof("[Total Info] Topic:%s Consumer:%s Partition:%d Total_Messages:%v Total_Lag:%v", topic, consumer, partition, total_messages, total_lag)
+	log.Infof("[Total Info] Topic:%v Consumer:%v Partition:%v Total_Messages:%v Total_Lag:%v Cluster:%s", topic, consumer, partition, total_messages, total_lag, mk.Cluster)
 }
 
 func NewZk(zks []string) (*zk.Conn, error) {
@@ -227,8 +286,11 @@ func GetConsumsers(zknode string, zkc *zk.Conn) []string {
 }
 
 func GetTopicsWithConsumser(zknode string, zkc *zk.Conn, consumer string) []string {
-	topics, _, err := zkc.Children(zknode + "/consumers/" + consumer + "/offsets")
-	checkErr(1, err)
+	getzknode := zknode + "/consumers/" + consumer + "/offsets"
+	topics, _, err := zkc.Children(getzknode)
+	if err != nil {
+		log.Errorf("get zknode %s error %v", getzknode, err)
+	}
 	return topics
 }
 
@@ -250,4 +312,29 @@ func checkErr(i int, err error) {
 func checkExist(filename string) bool {
 	_, err := os.Stat(filename)
 	return err == nil || os.IsExist(err)
+}
+
+func syncLogger() {
+
+	var LogConfig = `
+<seelog minlevel="trace">
+    <outputs formatid="common">
+        <rollingfile type="size" filename="` + cmdPath + `/../log/mkafka.log" maxsize="1000000" maxrolls="5"/>
+        <filter levels="critical">
+            <file path="` + cmdPath + `/../log/mkafka.log.wf" formatid="critical"/>
+        </filter>
+        <filter levels="warn">
+            <console formatid="colored"/>
+        </filter>
+    </outputs>
+    <formats>
+        <format id="colored"  format="%Time %EscM(46)%Level%EscM(49) %Msg%n%EscM(0)"/>
+        <format id="common" format="%Date/%Time [%LEV] [%File:%Line] %Msg%n" />
+        <format id="critical" format="%Date/%Time %File:%Line %Func %Msg%n" />
+    </formats>
+</seelog>
+`
+
+	logger, _ := log.LoggerFromConfigAsBytes([]byte(LogConfig))
+	log.UseLogger(logger)
 }
